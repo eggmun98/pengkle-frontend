71ac41e0a5bb645d0a925948448e2ca22bafb699
eggmun <eggmun98@gmail.com>
2025-01-28 21:37:16 +0900

    feat: 기본 메타 데이터 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,22 @@
-import "./globals.css";
+import { Metadata } from 'next';
+import './globals.css';
 
+export const metadata: Metadata = {
+  title: 'pengkle - 핑클 익명 SNS',
+  description: '익명 SNS에서 소통해보세요!',
+  icons: {
+    icon: [
+      {
+        url: '/images/logo.png',
+        sizes: '32x32',
+      },
+      {
+        url: '/images/logo.png',
+        sizes: '64x64',
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,10 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
