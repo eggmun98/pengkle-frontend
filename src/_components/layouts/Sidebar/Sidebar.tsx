04fbb39601ea1dd479d14ced844e0c554b3fe1f7
eggmun <eggmun98@gmail.com>
2025-01-29 16:53:47 +0900

    feat: 사이드 바 - Link 컴포넌트 적용 및 페이지 경로 설정

--- src/_components/layouts/Sidebar/Sidebar.tsx
+++ src/_components/layouts/Sidebar/Sidebar.tsx
@@ -2,14 +2,26 @@ import Home from '@/_components/icons/Home/Home';
 import Like from '@/_components/icons/Like/Like';
 import Profile from '@/_components/icons/Profile/Profile';
 import Setting from '@/_components/icons/Setting/Setting';
+import Link from 'next/link';
 
 const Sidebar = () => {
   return (
     <div className="flex flex-col items-center mt-20 gap-20">
-      <Home />
-      <Like />
-      <Profile />
-      <Setting />
+      <Link href="/" className="hover:opacity-50 transition-opacity cursor-pointer">
+        <Home />
+      </Link>
+
+      <Link href="/likes" className="hover:opacity-50 transition-opacity cursor-pointer">
+        <Like />
+      </Link>
+
+      <Link href="/profile" className="hover:opacity-50 transition-opacity cursor-pointer">
+        <Profile />
+      </Link>
+
+      <Link href="/settings" className="hover:opacity-50 transition-opacity cursor-pointer">
+        <Setting />
+      </Link>
     </div>
   );
 };
